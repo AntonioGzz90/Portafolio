cac04f2e859ed406000bb0dd1fd46802b98aa3c9
AntonioGzz90 <61254309+AntonioGzz90@users.noreply.github.com>
2023-09-04 16:08:03 -0600

    Archivo para abrir los modales

---
+++ js/modal.js
@@ -0,0 +1,56 @@
+const abrirModal1 = document.querySelector("#abrir-modal1");
+const cerrarModal1 = document.querySelector("#cerrar-modal1");
+const modal1 = document.querySelector("#modal1");
+
+abrirModal1.addEventListener("click",()=>{modal1.showModal();})
+
+cerrarModal1.addEventListener("click",()=>{modal1.close();})
+
+
+const abrirModal2 = document.querySelector("#abrir-modal2");
+const cerrarModal2 = document.querySelector("#cerrar-modal2");
+const modal2 = document.querySelector("#modal2");
+
+abrirModal2.addEventListener("click",()=>{modal2.showModal();})
+
+cerrarModal2.addEventListener("click",()=>{modal2.close();})
+
+
+const abrirModal3 = document.querySelector("#abrir-modal3");
+const cerrarModal3 = document.querySelector("#cerrar-modal3");
+const modal3 = document.querySelector("#modal3");
+
+abrirModal3.addEventListener("click",()=>{modal3.showModal();})
+
+cerrarModal3.addEventListener("click",()=>{modal3.close();})
+
+
+
+const abrirModal4 = document.querySelector("#abrir-modal4");
+const cerrarModal4 = document.querySelector("#cerrar-modal4");
+const modal4 = document.querySelector("#modal4");
+
+abrirModal4.addEventListener("click",()=>{modal4.showModal();})
+
+cerrarModal4.addEventListener("click",()=>{modal4.close();})
+
+
+
+const abrirModal5 = document.querySelector("#abrir-modal5");
+const cerrarModal5 = document.querySelector("#cerrar-modal5");
+const modal5 = document.querySelector("#modal5");
+
+abrirModal5.addEventListener("click",()=>{modal3.showModal();})
+
+cerrarModal5.addEventListener("click",()=>{modal5.close();})
+
+
+const abrirModal6 = document.querySelector("#abrir-modal6");
+const cerrarModal6 = document.querySelector("#cerrar-modal6");
+const modal6 = document.querySelector("#modal6");
+
+abrirModal6.addEventListener("click",()=>{modal6.showModal();})
+
+cerrarModal6.addEventListener("click",()=>{modal6.close();})
+
+
